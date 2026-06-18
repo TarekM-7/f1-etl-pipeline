b5f1f16b851c009a810c27df9f372fb3ff55cc8d
Tarek Mariscal <tarekmrscl@gmail.com>
2026-06-17 20:23:42 -0600

    fix: add error handling to loading process

--- etl/load.py
+++ etl/load.py
@@ -1,10 +1,27 @@
 import os
 from dotenv import load_dotenv
 import boto3
+import logging
+
+logging.basicConfig(
+    format='[{levelname}] {asctime} | {message}',
+    style='{',
+    datefmt='%H:%M:%S',
+    level=logging.INFO
+)
+logger = logging.getLogger(__name__)
 
 def load(df, local_path, s3_key):
-    load_dotenv()
-    bucket_name = os.getenv('AWS_BUCKET_NAME')
-    df.to_csv(local_path, index=False)
-    s3 = boto3.client('s3')
-    s3.upload_file(local_path, bucket_name, s3_key)
+    try:
+        logger.info("Starting data load stage")
+        load_dotenv()
+        bucket_name = os.getenv('AWS_BUCKET_NAME')
+        logger.info("Writing processed dataset to local storage")
+        df.to_csv(local_path, index=False)
+        logger.info("Uploading processed dataset to S3")
+        s3 = boto3.client('s3')
+        s3.upload_file(local_path, bucket_name, s3_key)
+        logger.info("Data load stage completed successfully")
+    except Exception as e:
+        logger.error(f"Error loading the data: {e}")
+        raise
